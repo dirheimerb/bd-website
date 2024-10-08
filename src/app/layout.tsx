6f68c18f94bb704d5b54541456ea3077d057d80c
Bradley Dirheimer <b.dirheimer@gmail.com>
2024-10-08 11:06:09 -0400

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,17 @@ import { type Metadata } from 'next';
 
 import { Providers } from '@/app/providers';
 import { Layout } from '@/components/Layout';
+import { Analytics } from "@vercel/analytics/react"
 
 import '@/styles/tailwind.css';
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - Spencer Sharp',
-    default: 'Spencer Sharp - Software designer, founder, and amateur astronaut',
+    template: '%s - Bradley Dirheimer',
+    default: 'Bradley Dirheimer - Senior Consultant and Software Engineer',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'I\'m Bradley Dirheimer, a Senior Consultant and Software Engineer based in Hamilton, Ohio. I specialize in Salesforce, data platforms, and building cutting-edge solutions that optimize business processes and improve customer experiences.',
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
@@ -25,7 +26,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
           <div className="flex w-full">
-            <Layout>{children}</Layout>
+            <Layout>
+              {children}
+              <Analytics />
+            </Layout>
           </div>
         </Providers>
       </body>
